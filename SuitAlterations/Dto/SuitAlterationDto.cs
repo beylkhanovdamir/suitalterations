3b41c11fc271c1b8005a9c63b25213539233f76a
Damir Beylkhanov <dbeylkhanov@appulate.com>
2019-11-24 19:08:13 +0300

    set UI friendly error message for alteration validation

--- SuitAlterations/Dto/SuitAlterationDto.cs
+++ SuitAlterations/Dto/SuitAlterationDto.cs
@@ -4,13 +4,13 @@ using SuitAlterations.Core.Common;
 namespace SuitAlterations.Dto {
 	public class SuitAlterationDto {
 		public int Id { get; set; }
-		[Range(-5, 5), Required]
+		[Range(-5, 5, ErrorMessage = "Can be up to plus or minus 5 cm"), Required(ErrorMessage = "This field is required")]
 		public int LeftSleeveLength { get; set; }
-		[Range(-5, 5), Required]
+		[Range(-5, 5, ErrorMessage = "Can be up to plus or minus 5 cm"), Required(ErrorMessage = "This field is required")]
 		public int RightSleeveLength { get; set; }
-		[Range(-5, 5), Required]
+		[Range(-5, 5, ErrorMessage = "Can be up to plus or minus 5 cm"), Required(ErrorMessage = "This field is required")]
 		public int LeftTrouserLength { get; set; }
-		[Range(-5, 5), Required]
+		[Range(-5, 5, ErrorMessage = "Can be up to plus or minus 5 cm"), Required(ErrorMessage = "This field is required")]
 		public int RightTrouserLength { get; set; }
 		public SuitAlterationStatus Status { get; set; }
 		public string AlterationTitle { get; set; }
